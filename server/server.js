ec2591036696fbaf2c557f9f839ba097d007ba16
killbyfire <killbyfiredev@gmail.com>
2024-11-23 23:11:29 +0100

    Organize file structure - Work on socket.io ~ WIP

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const publicFolder = "../public/";
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, publicFolder, "index.html"));
+});
+
+app.listen(3000);
