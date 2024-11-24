0fe85f6e9f48c0f359f0a9ac4eff34277487b6c2
killbyfire <killbyfiredev@gmail.com>
2024-11-24 23:08:01 +0100

    Add room api and fix players not registering in rooms ~ Feat

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const publicFolder = "../public/";
+const apiPath = "/api/v1/";
 
 app.use(express.static(path.join(__dirname, publicFolder)));
 
@@ -22,11 +26,23 @@ io.on("connection", (socket) => {
     console.log(
       "A new room has been created with the ID: " + roomDetails.roomID
     );
+
     currentRooms.push({
       roomID: roomDetails.roomID,
-      players: [roomDetails.userID],
+      players: [roomDetails.playerID],
     });
   });
+});
+
+app.post(apiPath + "rooms/players", function (req, res) {
+  const roomID = req.body.roomID ? req.body.roomID : null;
+  if (!roomID) {
+    res.send({ status: "No room found" });
+  }
+
+  const foundRoom = currentRooms.filter((room) => room.roomID === roomID);
+
+  res.send({ players: foundRoom[0].players });
 });
 
 app.get("/game/:gameID", function (req, res) {
